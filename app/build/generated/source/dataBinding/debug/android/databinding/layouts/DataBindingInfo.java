be2d7b6f3f206494b7a746ed4c73ef3922277ea3
Rafaela Macedo de Araujo <rafaela.araujo@supera.com.br>
2017-07-18 17:59:48 -0300

    removendo volley

--- app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package android.databinding.layouts;
-
-import android.databinding.BindingBuildInfo;
-
-@BindingBuildInfo(buildId="b4e23e86-0843-423c-9432-77df894b6606")
-public class DataBindingInfo {}
